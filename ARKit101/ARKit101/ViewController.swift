38c322ed7e95b1dc11cb1dfc4e9e8cad6e2e5a8f
adam smith <adaminhonolulu@gmail.com>
2017-08-14 14:51:28 -1000

    updates to ARKit101 vc

--- ARKit101/ARKit101/ViewController.swift
+++ ARKit101/ARKit101/ViewController.swift
@@ -4,71 +4,61 @@ import UIKit
 import SceneKit
 import ARKit
 
-class ViewController: UIViewController, ARSCNViewDelegate {
-
+class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
+    
+    private var planes = [OverlayPlane]()
     @IBOutlet var sceneView: ARSCNView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Set the view's delegate
         sceneView.delegate = self
-        
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
+        let scene = SCNScene()
         sceneView.scene = scene
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
+        sceneView.addGestureRecognizer(tapGestureRecognizer)
     }
     
+    @objc private func handleTap(_ recognizer: UIGestureRecognizer) {
+        let postion = recognizer.location(in: sceneView)
+        
+        guard let hitResult = sceneView.hitTest(postion, types: .existingPlane).first else { return }
+        
+        let cubeNode = SCNNode(geometry: SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0))
+        let material = SCNMaterial()
+        material.diffuse.contents = UIColor.cyan
+        cubeNode.position = SCNVector3(hitResult.worldTransform.columns.3.x, hitResult.worldTransform.columns.3.y + 0.3, hitResult.worldTransform.columns.3.z)
+        sceneView.scene.rootNode.addChildNode(cubeNode)
+    }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-
-        // Run the view's session
+        configuration.planeDetection = .horizontal
         sceneView.session.run(configuration)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        
-        // Pause the view's session
         sceneView.session.pause()
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        guard let overlayPlane = planes.first(where: {$0.anchor.identifier == anchor.identifier}),
+            let anchor = anchor as? ARPlaneAnchor
+            else { return }
+        overlayPlane.update(anchor: anchor)
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        
+        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+        let planeNode = OverlayPlane(anchor: planeAnchor)
+        planes.append(planeNode)
+        node.addChildNode(planeNode)
+    }
+    
+    func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
+        
     }
 
-    // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
-    }
 }
